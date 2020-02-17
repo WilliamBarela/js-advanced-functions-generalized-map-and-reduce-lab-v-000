b24322e613baa3ded13d00991dd7aced8ce719c2
William Barela <william.barela.dev@gmail.com>
2020-02-17 00:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ const map = (arr, f) => {
 };
 
 const reduce = (arr, f, startingPoint = 0) => {
+  reducedVal = startingPoint
   
+  for(const elem of arr){
+    reducedVal += elem
+  }
+  return r
 }
